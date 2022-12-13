93b9eea70adfc64c5fd5e6b28b395f61c57377c9
Andrey <aimakar@mail.ru>
2022-12-13 12:56:25 +0300

    Lesson 8

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,18 @@
 package org.example;
 
-public class ClassicalMusic implements Music{
+public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+    public void doInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doDestruction() {
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong() {
         return "Sonata";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,20 +7,25 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+        ClassicalMusic classicalMusic = context.getBean("classicalMusicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic2.getSong());
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(comparison);
-
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(10);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 //        firstMusicPlayer.playMusic();
 //        System.out.println(firstMusicPlayer.getName());
 //        System.out.println(firstMusicPlayer.getVolume());
