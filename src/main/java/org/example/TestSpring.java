88ac1463afc3919c9f61d55502f513161b179b32
Andrey <aimakar@mail.ru>
2022-12-14 14:30:42 +0300

    Lesson 10

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,14 +7,19 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
 
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer musicPlayer1 = new MusicPlayer(music2);
-        musicPlayer1.playMusic();
+//        Music music = context.getBean("rockMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer musicPlayer1 = new MusicPlayer(music2);
+//        musicPlayer1.playMusic();
 
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
